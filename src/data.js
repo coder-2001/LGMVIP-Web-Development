7146dcc128a773b4ef0e00c9d4d2a1aacae8b59a
@coder-2001 <baloda.bhupesh28@gmail.com>
2021-08-25 17:48:44 +0530

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,50 @@
+  // "page": 1,
+  // "per_page": 6,
+  // "total": 12,
+  // "total_pages": 2,
+// const page = 1;
+// const per_page = 6;
+// const total = 12;
+// const total_pages = 2;
+const data = [
+  {
+      email: "george.bluth@reqres.in",
+      first_name: "George",
+      last_name: "Bluth",
+      avatar: "https://reqres.in/img/faces/1-image.jpg"
+    },
+    {
+     
+      email: "janet.weaver@reqres.in",
+      first_name: "Janet",
+      last_name: "Weaver",
+      avatar: "https://reqres.in/img/faces/2-image.jpg"
+    },
+    {
+   
+      email: "emma.wong@reqres.in",
+      first_name: "Emma",
+      last_name: "Wong",
+      avatar: "https://reqres.in/img/faces/3-image.jpg"
+    },
+    {
+
+      email: "eve.holt@reqres.in",
+      first_name: "Eve",
+      last_name: "Holt",
+      avatar: "https://reqres.in/img/faces/4-image.jpg"
+    },
+    {
+      email: "charles.morris@reqres.in",
+      first_name: "Charles",
+      last_name: "Morris",
+      avatar: "https://reqres.in/img/faces/5-image.jpg"
+    },
+    {
+   
+      email: "tracey.ramos@reqres.in",
+      first_name: "Tracey",
+      last_name: "Ramos",
+      avatar: "https://reqres.in/img/faces/6-image.jpg"
+    }
+]
